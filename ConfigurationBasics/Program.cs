4e33a2d030f52301ba8d7576b54a57e8b6e107e9
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-09-04 02:50:27 +0300

    AddInMemoryCollection

--- ConfigurationBasics/Program.cs
+++ ConfigurationBasics/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConfigurationBasics
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationBasics
 {
     internal class Program
     {
         private static void Main()
         {
+            var settings = new Dictionary<string, string>
+            {
+                ["HelloMessage"] = "Hello, World",
+                ["GoodbyeMessage"] = "Goodbye, World"
+            };
 
+            var builder = new ConfigurationBuilder();
+            builder.AddInMemoryCollection(settings);
+
+            var config = builder.Build();
+
+            Console.WriteLine($"{config["HelloMessage"]}");
+            Console.WriteLine($"{config["GoodbyeMessage"]}");
         }
     }
 }
